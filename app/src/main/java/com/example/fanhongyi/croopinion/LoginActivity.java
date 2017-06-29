8637b56f887793aadf7b572693d96f5f32f704a7
FANHONGYI <fan_hongyi@qq.com>
2017-06-29 08:53:56 +0800

    //08:53
    增加记住密码和自动登录功能

--- app/src/main/java/com/example/fanhongyi/croopinion/LoginActivity.java
+++ app/src/main/java/com/example/fanhongyi/croopinion/LoginActivity.java
@@ -1,34 +1,85 @@
 package com.example.fanhongyi.croopinion;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String user="a";
-    private static final String pass="a";
-    private EditText username=null;
-    private EditText  password=null;
-    //private Button login;
+    private static final String user="admin",pass="123";
+    private EditText username=null,password=null;
+    private CheckBox remember,autologin;
+    private SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         username = (EditText)findViewById(R.id.editText1);
         password = (EditText)findViewById(R.id.editText2);
-        //login = (Button)findViewById(R.id.button1);
+        remember = (CheckBox) findViewById(R.id.remember);
+        autologin = (CheckBox) findViewById(R.id.autologin);
+
+        sp = getSharedPreferences("userInfo", 0);
+        String name=sp.getString("USER_NAME", "");
+        String pass =sp.getString("PASSWORD", "");
+
+        boolean choseRemember =sp.getBoolean("remember", false);
+        boolean choseAutoLogin =sp.getBoolean("autologin", false);
+
+        //如果上次选了记住密码，那进入登录页面也自动勾选记住密码，并填上用户名和密码
+        if(choseRemember){
+            username.setText(name);
+            password.setText(pass);
+            remember.setChecked(true);
+        }
+        //如果上次登录选了自动登录，那进入登录页面也自动勾选自动登录
+        if(choseAutoLogin){
+            autologin.setChecked(true);
+            
+            Toast.makeText(getApplicationContext(), "Logged in",
+                    Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent();
+            intent.setClass(LoginActivity.this, MainActivity.class);
+            intent.putExtra("u", username.getText().toString());
+            startActivity(intent);
+        }
+
     }
 
     public void login(View view){
+        SharedPreferences.Editor editor =sp.edit();
         if(username.getText().toString().equals(user) &&
                 password.getText().toString().equals(pass)){
+
+            //保存用户名和密码
+            editor.putString("USER_NAME", username.getText().toString());
+            editor.putString("PASSWORD", password.getText().toString());
+
+            //是否记住密码
+            if(remember.isChecked()){
+                editor.putBoolean("remember", true);
+            }else{
+                editor.putBoolean("remember", false);
+            }
+
+
+            //是否自动登录
+            if(autologin.isChecked()){
+                editor.putBoolean("autologin", true);
+            }else{
+                editor.putBoolean("autologin", false);
+            }
+            editor.commit();
+
             Toast.makeText(getApplicationContext(), "Logged in",
                     Toast.LENGTH_SHORT).show();
             Intent intent = new Intent();
