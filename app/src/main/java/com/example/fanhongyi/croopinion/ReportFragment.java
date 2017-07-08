47595cf96c4e94f4626f72d46d0b5a2ceb2b7db0
FANHONGYI <fan_hongyi@qq.com>
2017-07-08 22:47:21 +0800

    //22:47

--- app/src/main/java/com/example/fanhongyi/croopinion/ReportFragment.java
+++ app/src/main/java/com/example/fanhongyi/croopinion/ReportFragment.java
@@ -58,6 +58,7 @@ public class ReportFragment extends Fragment{
         showPieChart(mPieChart, mPieData);
 
         mLineChart = (LineChart) view.findViewById(lineChart);
+        dataSets.clear();
         mLineChart.setData(getLineData("test"));
         mLineChart.notifyDataSetChanged();
         showLineChart();
@@ -237,56 +238,15 @@ public class ReportFragment extends Fragment{
 
         // modify the legend ...
         // mLegend.setPosition(LegendPosition.LEFT_OF_CHART);
-        mLegend.setEnabled(false);
-        mLegend.setForm(Legend.LegendForm.CIRCLE);// 样式
+        mLegend.setEnabled(true);
+        mLegend.setForm(Legend.LegendForm.SQUARE);// 样式
         mLegend.setFormSize(6f);// 字体
-        mLegend.setTextColor(Color.WHITE);// 颜色
+        mLegend.setTextColor(Color.BLACK);// 颜色
 //      mLegend.setTypeface(mTf);// 字体
 
         mLineChart.animateX(2500); // 立即执行的动画,x轴
     }
 
-//    /**
-//     * 生成一个数据
-//     * @param count 表示图表中有多少个坐标点
-//     * @param range 用来生成range以内的随机数
-//     * @return
-//     */
-//    private LineData getLineData(int count, float range) {
-//        ArrayList<String> xValues = new ArrayList<String>();
-//        for (int i = 0; i < count; i++) {
-//            // x轴显示的数据，这里默认使用数字下标显示
-//            xValues.add("" + i);
-//        }
-//
-//        // y轴的数据
-//        ArrayList<Entry> yValues = new ArrayList<Entry>();
-//        for (int i = 0; i < count; i++) {
-//            float value = (float) (Math.random() * range) + 3;
-//            yValues.add(new Entry(value, i));
-//        }
-//
-//        // create a dataset and give it a type
-//        // y轴的数据集合
-//        LineDataSet lineDataSet = new LineDataSet(yValues, "测试折线图" /*显示在比例图上*/);
-//        // mLineDataSet.setFillAlpha(110);
-//        // mLineDataSet.setFillColor(Color.RED);
-//
-//        //用y轴的集合来设置参数
-//        lineDataSet.setLineWidth(1.75f); // 线宽
-//        lineDataSet.setCircleSize(3f);// 显示的圆形大小
-//        lineDataSet.setColor(Color.BLACK);// 显示颜色
-//        lineDataSet.setCircleColor(Color.BLACK);// 圆形的颜色
-//        lineDataSet.setHighLightColor(Color.BLACK); // 高亮的线的颜色
-//
-//        ArrayList<LineDataSet> lineDataSets = new ArrayList<LineDataSet>();
-//        lineDataSets.add(lineDataSet); // add the datasets
-//
-//        // create a data object with the datasets
-//        LineData lineData = new LineData(xValues, lineDataSet);
-//
-//        return lineData;
-//    }
     private LineData getLineData(String s){
         //Log.i("testStrings",s);
         LineDataSet dataSet=new LineDataSet(getChartData(DATA_COUNT), s);
